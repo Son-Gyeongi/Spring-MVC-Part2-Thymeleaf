40c7475be866f9a70660fe6e51dffa87a45fc230
Son-Gyeongi <thsruddl77@gmail.com>
2023-08-04 18:29:08 +0900

    타임리프-기본기능 / 텍스트 출력 기본

---
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,25 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        // 타입리프 뷰 템플릿에 데이터를 넘긴다.
+        model.addAttribute("data", "Hello Spring!");
+        return "basic/text-basic"; // 타입리프 뷰 템플릿
+    }
+
+    @GetMapping("/text-unescaped")
+    public String textUnescaped(Model model) {
+        // 타입리프 뷰 템플릿에 데이터를 넘긴다.
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped"; // 타입리프 뷰 템플릿
+    }
+}
