d31c773ba830ed03196c90152f7e1063cb8348d2
Son-Gyeongi <thsruddl77@gmail.com>
2023-08-04 18:46:25 +0900

    타임리프-기본기능 / 변수 SpringEL

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package hello.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -22,5 +28,37 @@ public class BasicController {
         // 타입리프 뷰 템플릿에 데이터를 넘긴다.
         model.addAttribute("data", "Hello <b>Spring!</b>");
         return "basic/text-unescaped"; // 타입리프 뷰 템플릿
+    }
+
+    // SpringEL
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+    // variable() - 샘플 데이터를 위해서 User클래스를 내부에서 만들어서 쓰겠다.
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
