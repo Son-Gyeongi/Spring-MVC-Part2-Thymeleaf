da0aa9ddcb41ef9ae6527ec52eb30257ff247682
Son-Gyeongi <thsruddl77@gmail.com>
2023-08-05 20:12:23 +0900

    edit. 타임리프-기본기능 / 유틸리티 객체와 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -87,5 +88,10 @@ public class BasicController {
         }
     }
 
-
+    // 유틸리티 객체와 날짜
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now()); // 현재 시간
+        return "basic/date";
+    }
 }
